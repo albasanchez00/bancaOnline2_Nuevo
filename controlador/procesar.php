d47b9058b2bb4d5b61ef0a20ab0413f3061618f5
albasanchez00 <albasanchezolmedo00@gmail.com>
2024-10-03 20:04:52 +0200

    Initial commit

---
+++ controlador/procesar.php
@@ -0,0 +1,54 @@
+<?php
+require_once '../mod/UsuarioBanco.php';
+use modell\UsuarioBanco;
+session_start();
+// Inicializar el usuario en la sesión si no existe
+if (!isset($_SESSION['usuario'])) {
+    $_SESSION['usuario'] = new UsuarioBanco('Juan', 1234);
+}
+// Verificar qué acción se debe ejecutar
+if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+
+    switch ($action) {
+        case 'validar':
+            $nombre = $_POST['nombre'];
+            $pin = $_POST['pin'];
+            if ($_SESSION['usuario']->validaUsuario($nombre, $pin)) {
+                header ("Location:../files/menuOpciones.php");
+            } else {
+                header("Location:../index.php?mensaje=Nombre o PIN incorrectos.");
+            }
+            break;
+
+        case 'cambiar_pin':
+            $nuevoPin = $_POST['nuevoPin'];
+            $_SESSION['usuario']->cambiaPin($nuevoPin);
+            echo "PIN cambiado correctamente.";
+            header ("Location:../files/menuOpciones.php");
+
+            break;
+
+        case 'ingresar':
+            $cantidad = $_POST['cantidad'];
+            $_SESSION['usuario']->ingresar($cantidad);
+            echo "Cantidad ingresada correctamente.";
+            header ("Location:../files/menuOpciones.php");
+
+            break;
+
+        case 'sacar':
+            $cantidadSacar = $_POST['cantidadSacar'];
+            if ($_SESSION['usuario']->sacar($cantidadSacar)) {
+                echo "Cantidad retirada correctamente.";
+                header ("Location:../files/menuOpciones.php");
+            } else {
+                echo "Saldo insuficiente para retirar esa cantidad.";
+            }
+            break;
+    }
+}
+
+//// Redirigir al index después de procesar
+//header("Location: ../index.php");
+//exit();
---
+++ files/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Banca Online</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style2.css">
+    <link rel="icon" href="media/favicon.jpg">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+</head>
+<body>
+<div class="row ancho">
+    <div class="col-6">
+        <img src="media/logo-banca-online.png" alt="Banca Online" class="logo">
+    </div>
+    <div class="col-6  derecha">
+            <span class="material-symbols-outlined icono-verde">
+                contact_support
+            </span>
+        <span class="ayuda">
+                ¿Te ayudamos?
+            </span>
+    </div>
+</div>
